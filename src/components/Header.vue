efff217674ac9c4b6c51f320b1c90edc0ea2416e
Angelo7x <abennici96@gmail.com>
2021-10-02 17:31:52 +0200

    Inserito header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-      
+      <img src="https://loghi-famosi.com/wp-content/uploads/2020/09/Logo-della-Spotify.png" alt="logo">
   </header>
 </template>
 
@@ -11,5 +11,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  header {
+    background-color: #505d6b;
+    img {
+      width: 100px;
+      padding: 10px;
+    }
+  }
 </style>
